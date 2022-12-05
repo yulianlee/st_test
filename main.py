f07fb84ca51a2f22f397e366e5f5225212703a39
leeyinghern <leeyinghern@gmail.com>
2022-12-05 12:34:26 +0800

    FML

--- main.py
+++ main.py
@@ -10,32 +10,37 @@ class Template:
     def __init__(self):
         self.ans_1 = st.text_input("Q1: 先生のお仕事は何ですか？")
         self.button_1 = st.button("答え")
-        self.placeholder_1 = st.empty()
         self.ans_2 = st.text_input("Q2: 一番苦手な生徒の国籍は？")
         self.button_2 = st.button("答え",key=2)
-        self.placeholder_2 = st.empty()
         self.ans_3 = st.text_input("Q3: 先生の好きなポケモンの名前は何ですか？")
         self.button_3 = st.button("答え",key=3)
-        self.placeholder_3 = st.empty()
         self.ans_1_accepted = ["営業仕事","えいぎょうしごと","営業","エイギョウ"]
         self.ans_2_accepted = ["中国","ちゅうごく","チュウゴク"]
         self.ans_3_accepted = ["イーブイ"]
+        self.header = st.header("")
+
+        if 'checker' not in st.session_state:
+            st.session_state.checker = 0
     
-    def render(self):
-        self.ans_1
-        self.button_1
-        self.placeholder_1     
+    def render1(self):
+        if st.session_state.checker == 0:
+            self.ans_1
+            self.button_1
 
-        st.header("")
-        self.ans_2
-        self.button_2
-        self.placeholder_2
+    def render2(self):
+        if st.session_state.checker == 1:
+            self.header
+            self.ans_2
+            self.button_2
 
-        st.header("")
-        self.ans_3 
-        self.button_3
-        self.placeholder_3
+    def render3(self):
+        if st.session_state.checker == 2:
+            self.header
+            self.ans_3 
+            self.button_3
 
+        self.header
+        st.subheader(f"正しい答え: {st.session_state.checker}")
 
     def find_substring(self,lst,string):
         for i in lst:
@@ -44,42 +49,50 @@ class Template:
         else:
             return False
 
+    def render_imgs(self):
+        st.image("Images/job.jpg")
+        
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image("Images/doubt.jpg")
+        with col2:
+            st.image("Images/doubt2.jpg")
 
-    def render_template(self,accepted_ans, button, ans, img_path, placeholder):
-        # print(prev_corr)
+        st.image("Images/pokemon.jpg")
+        st.image("Images/final.jpg")
+        return 
+
+    def render_template(self,accepted_ans, button, ans):
+  
         if button:
             if ans in accepted_ans:
-                with placeholder:
-                    st.image(img_path)
+                st.session_state.checker = st.session_state.checker + 1 if st.session_state.checker <=3 else st.session_state.checker
+                if st.session_state.checker >3:
+                    self.render_imgs()
                 return True
             else:
-                st.text("間違いました。もう１回答えてください。")
+                st.subheader("間違いました。もう１回答えてください。")
             return False
     
-    # elif not prev_corr:
-    #     st.header("")
-    #     st.subheader("前の質問を答えてください。")
 
 
 if __name__ == '__main__':
     obj = Template()
-    obj.render()
     
     if obj.button_1:
         obj.render_template(obj.ans_1_accepted,
-        obj.button_1,obj.ans_1,
-        "Images/job.jpg",
-        obj.placeholder_1
-        )
-            
+                                    obj.button_1,
+                                    obj.ans_1,
+                                    )
+
     if obj.button_2:
-        obj.render_template(obj.ans_2_accepted,
-        obj.button_2,obj.ans_2,
-        "Images/doubt.jpg",
-        obj.placeholder_2)
+        res2 = obj.render_template(obj.ans_2_accepted,
+        obj.button_2,
+        obj.ans_2,
+        )
 
     if obj.button_3:
         obj.render_template(obj.ans_3_accepted,
-        obj.button_1,obj.ans_3,
-        "Images/job.jpg",
-        obj.placeholder_3)
+        obj.button_3,
+        obj.ans_3,
+        )
