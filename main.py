b747f43de650c54465acba231b745249830554bd
leeyinghern <leeyinghern@gmail.com>
2022-12-05 13:08:46 +0800

    Finally

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import streamlit as st
 
 st.set_page_config(layout="centered")
 
-st.subheader("手紙の内容を読むために、ブランクに正解の答えを入れてください。")
-st.header("")
 
 class Template:
 
@@ -14,8 +12,9 @@ class Template:
         self.button_2 = None
         self.ans_3 = None
         self.button_3 = None
+        self.feedback = st.empty()
         self.ans_1_accepted = ["営業仕事","えいぎょうしごと","営業","エイギョウ"]
-        self.ans_2_accepted = ["中国","ちゅうごく","チュウゴク"]
+        self.ans_2_accepted = ["中国","ちゅうごく","チュウゴク","中国人"]
         self.ans_3_accepted = ["イーブイ"]
         self.header = st.header("")
 
@@ -24,29 +23,34 @@ class Template:
     
     def render(self):
         if st.session_state.checker == 0:
+            st.subheader("手紙の内容を読むために、ブランクに正解の答えを入れてください。")
+            st.header("")
             self.ans_1 = st.text_input("Q1: 先生のお仕事は何ですか？")
-            self.button_1 = st.button("答え")
+            self.button_1 = st.button("答え", on_click=self.render_template, args = (self.ans_1_accepted,self.ans_1))
             self.header
             st.subheader(f"正しい答え: {st.session_state.checker}")
 
         if st.session_state.checker == 1:
+            st.subheader("手紙の内容を読むために、ブランクに正解の答えを入れてください。")
+            st.header("")
             self.header
             self.ans_2 = st.text_input("Q2: 一番苦手な生徒の国籍は？")
-            self.button_2 = st.button("答え",key=2)
+            self.button_2 = st.button("答え",key=2, on_click=self.render_template, args = (self.ans_2_accepted,self.ans_2))
             self.header
             st.subheader(f"正しい答え: {st.session_state.checker}")
 
         if st.session_state.checker == 2:
+            st.subheader("手紙の内容を読むために、ブランクに正解の答えを入れてください。")
+            st.header("")
             self.header
             self.ans_3 = st.text_input("Q3: 先生の好きなポケモンの名前は何ですか？")
-            self.button_3 = st.button("答え",key=3)
-
+            self.button_3 = st.button("答え",key=3, on_click=self.render_template, args = (self.ans_3_accepted,self.ans_3))
             self.header
             st.subheader(f"正しい答え: {st.session_state.checker}")
-            print(st.session_state.checker)
-        
-        if st.session_state.checker > 3:
-
+                
+        if st.session_state.checker >= 3:
+            st.header("今まで、どうもありがとうございます。")
+            st.header("")
             self.render_imgs()
 
     def find_substring(self,lst,string):
@@ -69,39 +73,12 @@ class Template:
         st.image("Images/final.jpg")
         return 
 
-    def render_template(self,accepted_ans, button, ans):
-  
-        if button:
-            if ans in accepted_ans:
-                st.session_state.checker = st.session_state.checker + 1 if st.session_state.checker <3 else st.session_state.checker
-                if st.session_state.checker >=3:
-                    self.render_imgs()
-                return True
-            else:
-                st.subheader("間違いました。もう１回答えてください。")
-            return False
-    
-
+    def render_template(self,accepted_ans,  ans):
+        if ans in accepted_ans:
+            st.session_state.checker = st.session_state.checker + 1 if st.session_state.checker <3 else st.session_state.checker
+        else: 
+            st.subheader("間違いました。もう１回答えてください。")
 
 if __name__ == '__main__':
     obj = Template()
     obj.render()
-
-    if obj.button_1:
-        obj.render_template(obj.ans_1_accepted,
-                                    obj.button_1,
-                                    obj.ans_1,
-                                    )
-        obj.render()
-    if obj.button_2:
-        res2 = obj.render_template(obj.ans_2_accepted,
-        obj.button_2,
-        obj.ans_2,
-        )
-        obj.render()
-    if obj.button_3:
-        obj.render_template(obj.ans_3_accepted,
-        obj.button_3,
-        obj.ans_3,
-        )
-        obj.render()
